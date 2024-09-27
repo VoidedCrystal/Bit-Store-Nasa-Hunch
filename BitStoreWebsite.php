7ba481ea5a593fbd54eccb2ace554d1c3a09cb20
Kyle Herman <Foxco2025@gmail.com>
2024-09-27 10:14:03 -0600

    Added php file

---
+++ BitStoreWebsite.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Untitled Document</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+	<h1>Check</h1>
+</body>
+</html>
