ab904b46f952e761457f0413b22133da3f00a854
leoke599 <leoke073@gmail.com>
2024-10-14 11:00:38 -0600

    added pop up box and formatted about

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,3 +9,14 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+
+/* Popup functions */
+window.addEventListener("load", function(){
+  setTimeout(
+      function open(event){
+          document.querySelector(".popup").style.display = "block";
+      }, 2000)
+});
+document.querySelector("#close").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none";
+});
