38d489c1d9c9d58708626a82dbf3f610c726f206
leoke599 <leoke073@gmail.com>
2024-10-15 04:28:10 +0000

    removed popup stuff will add back later

--- JS/scripts.js
+++ JS/scripts.js
@@ -11,12 +11,3 @@ function openNav() {
   }
 
 /* Popup functions */
-window.addEventListener("load", function(){
-  setTimeout(
-      function open(event){
-          document.querySelector(".popup").style.display = "block";
-      }, 2000)
-});
-document.querySelector("#close").addEventListener("click", function(){
-  document.querySelector(".popup").style.display = "none";
-});
