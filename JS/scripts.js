12667d5b6c5cb93e2c17ac55cf4d159594738257
leoke073 <leoke073@gmail.com>
2024-12-13 21:04:11 -0700

    intergrate into main test

---
+++ JS/scripts.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+/* Sidebar functions */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+/* Popup functions */
