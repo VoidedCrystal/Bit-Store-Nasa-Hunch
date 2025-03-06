3680acf94e6e716b2ce513f72a887fdf753d91c1
Kyle Herman <Foxco2025@gmail.com>
2025-03-06 09:50:18 -0700

    Added better look for invitations.

--- frontend/filehandling-frontend/src/Invitations.jsx
+++ frontend/filehandling-frontend/src/Invitations.jsx
@@ -3,6 +3,7 @@ import { useAuth } from './contexts/authContext';
 import { db } from './firebase/firebase';
 import { Link } from 'react-router-dom';
 import { collection, getDocs, updateDoc, doc, getDoc } from 'firebase/firestore';
+import './css/Invite.css';
 
 function Invitations() {
   const { logout } = useAuth();
@@ -100,10 +101,12 @@ function Invitations() {
       {invitations.length > 0 ? (
         <ul>
           {invitations.map(invitation => (
-            <li key={invitation.id}>
-              <p>Project Name: {projects[invitation.projectId] || 'Unknown Project'}</p>
-              <button onClick={() => acceptInvitation(invitation.id, invitation.projectId)}>Accept</button>
-            </li>
+            <div className="invitation">
+              <li key={invitation.id}>
+                <p>Project Name: {projects[invitation.projectId] || 'Unknown Project'}</p>
+                <button onClick={() => acceptInvitation(invitation.id, invitation.projectId)}>Accept</button>
+              </li>
+            </div>
           ))}
         </ul>
       ) : (
