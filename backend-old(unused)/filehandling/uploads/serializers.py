0d0b1eeb25ad4f4aae775df7fe29a9fe68874b31
Kyle Herman <Foxco2025@gmail.com>
2025-04-30 09:13:20 -0600

    Removed Backend Folder Altogether

--- backend-old(unused)/filehandling/uploads/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Document
-
-class DocumentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Document
-        fields = ['id', 'description', 'document', 'uploaded_at'] 
